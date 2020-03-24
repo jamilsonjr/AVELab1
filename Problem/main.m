ff7917b67467945f6964c2de06c8a812a4f8748e
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2020-03-24 23:57:00 +0000

    First exercise almost

--- Problem/main.m
+++ Problem/main.m
@@ -29,10 +29,45 @@ title('Basic urban driving cycle');
 xlabel('time $[s]$','Interpreter', 'latex');
 ylabel('Velocity [$v$]','Interpreter', 'latex');
 
+%% Vehicles Configurations and assumptions
+% In the study there are 4 possible configurations of  light duty urban vehicles, presented in
+% the figures (1), (2), (3) and (4). They consist of pure eletric, pure
+% combustion, series hybrid and paralel hybrid.
+%
+% <html><h3>Pure Eletric Powertrain</h3></html>
+% 
+% In this configuration the battery in series with the eletric
+% motor/generator in series with the transmission.
+% There are two possible modes of functioning, the motor discharging the
+% batteries while accelerating, and the opposite, generator charging the batteries decelerating.
+%% 
+% *Acceleration:*
+% During discharge the ciruit has an efficiency different of 100%. The overalll
+% efficiecy $\eta_{overall}$ is given by the product of the individual effinciency of
+% each component of the circuit
+%%
+% $\eta_{overall}=\prod_{i=1}\eta_i$. 
+%%
+% Thus, for this powertrain, one has that [$battery \rightarrow motor
+% \rightarrow transmission$] will translate in a overall efficiency given
+% by: 
+%%
+% $\eta_{overall}=\eta_{battery}\eta_{motor}\eta_{transmission}$
+%% 
+% * Assuming that the batteries are made from lithium-ion, according to
+% <https://en.wikipedia.org/wiki/Lithium-ion_battery> it is reasonable to assume that
+% the discharging effinciency of the battery of around 90%.
+% * From <https://en.wikipedia.org/wiki/Electric_car#Energy_efficiency> it
+% is reasonable to assume that the eletric motor has an efficiency of 70%.
+% * From <https://www.nap.edu/read/21744/chapter/7> it is resonable to
+% assume that the effinciecy of an automatic transmission is 90%.
+%% 
+% Thus the eletric power in acceleration is given by:
+%% 
+% $$Tesitng$$
 %% Power consumption
 % In this section it is of interest to compare the power consumption between
-% the different light duty urban vehicles presented in the figures (1), (2),
-% (3) and (4) of the Lab script.
+% the different vehicles.
 % 
 % The objective os to calculate:
 %% 
@@ -57,5 +92,7 @@ ylabel('Velocity [$v$]','Interpreter', 'latex');
 % surface area; $v_w\rightarrow$ wind speed; $C_{rr}\rightarrow$ rolling  
 % resistance coefficient; $m$ and $g$ mass a gravity;
 %%
-% Considering $f_m = 1.05$ and using the values form the table(I) of the
-% laboratory script, one can compute the values of $F_T$ of the value
+% Considering $f_m = 1.05$ and using the constant values from the table(I) of the
+% laboratory script, one can compute the values of $F_T$.
+
+
